184c1efa8787bea601d28a00b44b7487aee41f60
Lesley Cheung <lesley.yc@gmail.com>
2022-03-25 00:48:52 +0800

    done

---
+++ src/components/changeExt.js
@@ -0,0 +1,19 @@
+const ccc = require("clear_concise_creative")
+
+ccc.list()
+
+ccc.rename("./static", "jsx", "js")
+
+ccc.rename("./section", "jsx", "js")
+
+ccc.rename("./reference", "jsx", "js")
+
+ccc.rename("./pages", "jsx", "js")
+
+ccc.rename("./data", "jsx", "js")
+
+ccc.rename("./article", "jsx", "js")
+
+ccc.rename("./5_libz", "jsx", "js")
+
+ccc.rename("./6_layouts", "jsx", "js")
